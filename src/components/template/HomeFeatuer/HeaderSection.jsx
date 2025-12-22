aab06f93cbf05ba27186c1900984dff3df5636a6
mohsenical <mohsenical.web@gmail.com>
2025-12-22 22:02:16 +0330

    change section header

--- src/components/template/HomeFeatuer/HeaderSection.jsx
+++ src/components/template/HomeFeatuer/HeaderSection.jsx
@@ -5,14 +5,14 @@ import { MdOutlineKeyboardArrowLeft } from "react-icons/md"
 function HeaderSection() {
   return (
     <>
-      <div className=" bg-black min-h-screen md:min-h-[45rem] flex justify-center rounded-b-[3%]">
-        <div className=" flex items-center justify-center flex-col max-h-screen md:w-[80%] mt-2 lg:w-[55%] xl:w-[45%] z-0 bg-[url(/Image/bgAvatar.png)] bg-cover  md:bg-none">
-          <div className="flex flex-col mt-50 space-y-1 max-w-[80%] md:max-w-[55%] z-10">
+      <div className="min-h-[45rem] w-full px-30 bg-gray-300 dark:bg-zinc-900 border-b border-b-zinc-300 rounded-b-2xl ">
+        <div className=" flex ">
+
+          <div className="flex flex-col justify-center items-start space-y-1 h-[45rem] w-[50%] pt-50">
 
             <h2 className='text-2xl text-white md:text-zinc-600 '>سلام من</h2>
-            <h1 className='text-4xl text-green-700 mr-3 py-3 px-5 bg-black/50 w-[16rem] rounded-2xl '>محسن پیغامی</h1>
+            <h1 className='text-4xl text-green-700 mr-3 py-3 px-5 rounded-2xl '>محسن پیغامی</h1>
             <h2 className='text-2xl text-white md:text-zinc-600'>هستم .</h2>
-
 
             <p className='text-md text-white md:text-zinc-600 mt-6 font-bold leading-6'>
               یک طراح سایت زبده، ترکیبی از خلاقیت هنری، مهارت فنی پیشرفته و درک عمیق از تجربه کاربری (UX/UI) است.
@@ -28,11 +28,12 @@ function HeaderSection() {
                 <MdOutlineKeyboardArrowLeft />
               </button>
             </div>
-
           </div>
-        </div>
 
-        <div className="mt-[3rem] hidden lg:block lg:w-[45%] xl:w-[40%] h-[44rem] bg-[url(/Image/bgAvatar.png)] bg-cover">
+
+          <div className="flex items-center justify-center w-[50%] h-[45rem]">
+            <div className="w-[60%] h-[60%] pt-50 rounded-full border-4 border-green-900 bg-[url(/Image/Avatar.jpg)] bg-contain bg-cover bg-no-repeat"></div>
+          </div>
 
         </div>
       </div>
@@ -41,3 +42,13 @@ function HeaderSection() {
 }
 
 export default HeaderSection
+
+
+
+//   <div className=    z-10">
+
+
+
+
+
+//   </div>
